4fdc377a9da49fa3c20379c52a72c040c0a88dc1
Abramov Iurii <abtank@outlook.com>
2021-11-18 01:10:27 +0300

    builder

--- src/main/java/ru/abtank/java10/Main.java
+++ src/main/java/ru/abtank/java10/Main.java
@@ -1,9 +1,23 @@
 package ru.abtank.java10;
 
+import ru.abtank.java10.less1.Person;
+
 public class Main {
 
 
     public static void main(String[] args) {
         System.out.println("Hello World");
+
+        Person person = Person.builder()
+                .firstName("fn")
+                .lastName("ln")
+                .middleName("mn")
+                .country("ctr")
+                .address("add")
+                .phone("ph")
+                .age(12)
+                .gender("helicopter")
+                .build();
+        System.out.println(person);
     }
 }
